ea5ddcb2c2e8faf3f78038792c7db7aba17711be
Furkan <arabulfurkan@gmail.com>
2024-01-23 03:36:01 +0100

    add analytics

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,30 @@
 <template>
+
 	<div
 		v-if="appReady"
 		class="min-h-full font-Rubik box-border bg-gray-100 min-h-screen"
 	>
+
 		<Navigation />
+
 		<router-view />
+
+		<Analytics />
+
 	</div>
+
 </template>
 
 <script>
-import Navigation from "./components/Navigation.vue";
-import { ref } from "vue";
-import { supabase } from "./supabase/init";
-import store from "./store/index";
-export default {
-	components: {
-		Navigation
-	},
-	setup() {
-		const appReady = ref(null);
-
-		const user = supabase.auth.user();
-		if (!user) {
-			appReady.value = true;
-		}
-		supabase.auth.onAuthStateChange((_, session) => {
-			store.methods.setUser(session);
-			appReady.value = true;
-		});
-		return { appReady };
-	}
-};
+ import Navigation from "./components/Navigation.vue"; import { ref } from "vue";
+import { supabase } from "./supabase/init"; import store from "./store/index"; import
+{ Analytics } from '@vercel/analytics/react'; export default { components: { Navigation
+}, setup() { const appReady = ref(null); const user = supabase.auth.user(); if (!user)
+{ appReady.value = true; } supabase.auth.onAuthStateChange((_, session) => { store.methods.setUser(session);
+appReady.value = true; }); return { appReady, Analytics }; } };
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap');
 </style>
+
